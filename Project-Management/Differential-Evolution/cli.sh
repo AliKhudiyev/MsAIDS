ccca1812874a08c4fa391d69546a3d068c0b166c
AliKhudiyev <xudiyev-ali@mail.ru>
2020-12-11 15:45:05 +0400

    added AI practicals

--- Project-Management/Differential-Evolution/cli.sh
+++ Project-Management/Differential-Evolution/cli.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
+if [ ! -d build ]
+then
+    mkdir build
+    cd build && (cmake .. 1>/dev/null) && (make 1>/dev/null)
+fi
+
 if [ $1 == "ackley" ]
 then
-    # echo "ackley"
-    cd build && (cmake .. 1>/dev/null) && (make 1>/dev/null) && (./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b1)
+    ./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b1
 elif [ $1 == "rastrigin" ]
 then
-    # echo "rastrigin"
-    cd build && (cmake .. 1>/dev/null) && (make 1>/dev/null) && (./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b2)
+    ./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b2
 elif [ $1 == "rosenbrock" ]
 then
-    # echo "rosenbrock"
-    cd build && (cmake .. 1>/dev/null) && (make 1>/dev/null) && (./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b3)
+    ./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b3
 elif [ $1 == "schwefel" ]
 then
-    # echo "schwefel"
-    cd build && (cmake .. 1>/dev/null) && (make 1>/dev/null) && (./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b4)
+    ./main --global-min --population=30 --generation=10000 --benchmark-run=30 -o2 -b4
 else
-    # sed -i  '' "s/N_DIMENSION .*./N_DIMENSION $1/g" src/main.cpp
-    # src/set_function.sh "$2"
-    exit_status=0 # $?
-
-    if [ $exit_status == 0 ]
+    if [ $1 == "dea" ]
     then
-        if [ ! -d build ]
-        then
-            mkdir build
-        fi
-        cd build && (cmake .. 1>/dev/null) && (make 1>/dev/null) && (echo "$4" | ./main $3)
+        echo "$4" | ./main $3
     else
-        echo "Incorrent function!"
+        echo "$4" | ./ga $3
     fi
 fi
---
+++ Project-Management/Differential-Evolution/src/ga.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#include <vector>
+
+using input_t = std::vector<double>;
+using space_t = std::vector<input_t>;
+
+
